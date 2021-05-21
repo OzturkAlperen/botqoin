35d46cfd6d1d9ee76a693c0469548f16d187bffb
Alperen-Ozturk <ozturk.alperen@icloud.com>
2021-05-21 23:58:22 +0300

    convert kucoin api wrapper to async

--- backend/src/kucoin.py
+++ backend/src/kucoin.py
@@ -3,8 +3,8 @@ import hmac
 import hashlib
 import base64
 import time
-import requests
 import websockets
+import aiohttp
 
 
 class Client:
@@ -14,75 +14,77 @@ class Client:
         if passphrase:
             self.passphrase = base64.b64encode(hmac.new(secret.encode('utf-8'),
                                                         passphrase.encode('utf-8'),
-                                                        hashlib.sha256).digest())
-            self.session = self._init_session()
+                                                        hashlib.sha256).digest()).decode("utf8")
+        self.session = None
 
-    def _init_session(self):
-        session = requests.session()
+    async def close(self):
+        await self.session.close()
+
+    async def open(self):
+        self.session = await self._init_session()
+
+    async def _init_session(self):
         headers = {'Accept': 'application/json',
                    'User-Agent': 'botqoin',
                    'Content-Type': 'application/json',
                    'KC-API-KEY': self.key,
                    'KC-API-PASSPHRASE': self.passphrase,
                    "KC-API-KEY-VERSION": "2"}
-        session.headers.update(headers)
-        return session
+        return aiohttp.ClientSession(headers=headers)
 
-    def _create_signature(self, method, endpoint, data):
+    async def _create_signature(self, method, endpoint, data):
         now = int(time.time() * 1000)
         if data:
             string_to_sign = str(now) + method + endpoint + data
         else:
             string_to_sign = str(now) + method + endpoint
+
         signature = base64.b64encode(hmac.new(self.secret.encode('utf-8'),
                                               string_to_sign.encode('utf-8'),
-                                              hashlib.sha256).digest())
+                                              hashlib.sha256).digest()).decode("utf8")
         return now, signature
 
-    def _make_request(self, method, endpoint, **kwargs):
+    async def _make_request(self, method, endpoint, **kwargs):
         url = "https://api.kucoin.com{}".format(endpoint)
         data = {}
         for k, v in kwargs.items():
             data[k] = v
         data_json = json.dumps(data, separators=(',', ':'), ensure_ascii=False)
-        now, signature = self._create_signature(method, endpoint, data_json)
+        now, signature = await self._create_signature(method, endpoint, data_json)
         headers = {
             "KC-API-TIMESTAMP": str(now),
             "KC-API-SIGN": signature,
         }
-        try:
-            return self._process_response(getattr(self.session, method.lower())(url, headers=headers, data=data_json))
-        except requests.exceptions.RequestException as exception:
-            return exception
+        async with getattr(self.session, method.lower())(url, headers=headers, data=data_json) as response:
+            return await self._process_response(response)
 
     @staticmethod
-    def _process_response(response):
-
-        if not str(response.status_code).startswith('2'):
-            raise APIException(response)
+    async def _process_response(response):
+        if not str(response.status).startswith('2'):
+            raise KucoinAPIException(response, None)
         try:
-            response_json = response.json()
+            response_json = await response.json()
 
             if 'code' in response_json and response_json['code'] != "200000":
-                raise APIException(response)
+                raise KucoinAPIException(response, response_json)
 
             if 'success' in response_json and not response_json['success']:
-                raise APIException(response)
+                raise KucoinAPIException(response, response_json)
 
             if 'data' in response_json:
                 return response_json['data']
 
         except ValueError:
-            raise APIException(request_exception='Invalid Response: %s' % response.text, response=response)
+            return "INVALID RESPONSE"
 
-    def create_order(self,
-                     order_side: str,
-                     order_type: str,
-                     order_symbol: str,
-                     order_funds: str = None,
-                     order_size: str = None,
-                     order_price: str = None,
-                     order_clientoid: str = None):
+    async def create_order(self,
+                           order_side: str,
+                           order_type: str,
+                           order_symbol: str,
+                           order_funds: str = None,
+                           order_size: str = None,
+                           order_price: str = None,
+                           order_clientoid: str = None):
         if order_clientoid is not None:
             clientoid = order_clientoid
         else:
@@ -90,99 +92,94 @@ class Client:
 
         if order_type == "market":
             if order_funds is not None and order_size is None:
-                return self._make_request('POST', '/api/v1/orders',
-                                          clientOid=clientoid,
-                                          side=order_side,
-                                          type=order_type,
-                                          symbol=order_symbol,
-                                          funds=order_funds)
+                return await self._make_request('POST', '/api/v1/orders',
+                                                clientOid=clientoid,
+                                                side=order_side,
+                                                type=order_type,
+                                                symbol=order_symbol,
+                                                funds=order_funds)
             elif order_funds is None and order_size is not None:
-                return self._make_request('POST', '/api/v1/orders',
-                                          clientOid=clientoid,
-                                          side=order_side,
-                                          type=order_type,
-                                          symbol=order_symbol,
-                                          size=order_size)
+                return await self._make_request('POST', '/api/v1/orders',
+                                                clientOid=clientoid,
+                                                side=order_side,
+                                                type=order_type,
+                                                symbol=order_symbol,
+                                                size=order_size)
             else:
                 return "Error @ order_size/order_funds"
         elif order_type == "limit":
             if order_funds is not None and order_size is None:
-                return self._make_request('POST', '/api/v1/orders',
-                                          clientOid=clientoid,
-                                          side=order_side,
-                                          type=order_type,
-                                          symbol=order_symbol,
-                                          funds=order_funds,
-                                          price=order_price)
+                return await self._make_request('POST', '/api/v1/orders',
+                                                clientOid=clientoid,
+                                                side=order_side,
+                                                type=order_type,
+                                                symbol=order_symbol,
+                                                funds=order_funds,
+                                                price=order_price)
             elif order_funds is None and order_size is not None:
-                return self._make_request('POST', '/api/v1/orders',
-                                          clientOid=clientoid,
-                                          side=order_side,
-                                          type=order_type,
-                                          symbol=order_symbol,
-                                          funds=order_funds,
-                                          size=order_size,
-                                          price=order_price)
+                return await self._make_request('POST', '/api/v1/orders',
+                                                clientOid=clientoid,
+                                                side=order_side,
+                                                type=order_type,
+                                                symbol=order_symbol,
+                                                funds=order_funds,
+                                                size=order_size,
+                                                price=order_price)
             else:
                 return "Error @ order_size/order_funds"
         else:
             return "Error @ order_type"
 
-    def cancel_order(self, order_id: str = None, order_clientoid: str = None):
+    async def cancel_order(self, order_id: str = None, order_clientoid: str = None):
         if order_id is not None:
-            return self._make_request('DELETE', '/api/v1/orders/{}'.format(order_id))
+            return await self._make_request('DELETE', '/api/v1/orders/{}'.format(order_id))
         elif order_clientoid is not None:
-            return self._make_request('DELETE', '/api/v1/order/client-order/{}'.format(order_clientoid))
+            return await self._make_request('DELETE', '/api/v1/order/client-order/{}'.format(order_clientoid))
         else:
             return "Error -> Provide either order_id or order_clientoid"
 
-    def get_order(self, order_id: str = None, order_clientoid: str = None):
+    async def get_order(self, order_id: str = None, order_clientoid: str = None):
         if order_id is not None:
-            return self._make_request('GET', '/api/v1/orders/{}'.format(order_id))
+            return await self._make_request('GET', '/api/v1/orders/{}'.format(order_id))
         elif order_clientoid is not None:
-            return self._make_request('GET', '/api/v1/order/client-order/{}'.format(order_clientoid))
+            return await self._make_request('GET', '/api/v1/order/client-order/{}'.format(order_clientoid))
         else:
             return "Error -> Provide either order_id or order_clientoid"
 
-    def get_symbols(self):
-        return self._make_request('GET', '/api/v1/symbols')
+    async def get_symbols(self):
+        return await self._make_request('GET', '/api/v1/symbols')
 
 
-class APIException(Exception):
-    def __init__(self, response, request_exception=None):
+class KucoinAPIException(Exception):
+    def __init__(self, response, response_json):
 
-        self.request_exception = request_exception
         self.code = 'Unknown'
         self.message = 'Unknown'
 
-        try:
-            response_json = response.json()
-        except ValueError:
-            self.message = response.content
-        else:
-            if 'error' in response_json:
+        if response_json is not None:
+            self.response_json = response_json
+            if 'error' in self.response_json:
                 self.message = response['error']
-            if 'msg' in response_json:
-                self.message = response_json['msg']
-            if 'message' in response_json and response_json['message'] != 'No message available':
-                self.message += ' - {}'.format(response_json['message'])
-            if 'code' in response_json:
-                self.code = response_json['code']
-            if 'data' in response_json:
+            if 'msg' in self.response_json:
+                self.message = self.response_json['msg']
+            if 'message' in self.response_json and self.response_json['message'] != 'No message available':
+                self.message += ' - {}'.format(self.response_json['message'])
+            if 'code' in self.response_json:
+                self.code = self.response_json['code']
+            if 'data' in self.response_json:
                 try:
-                    self.message += " " + json.dumps(response_json['data'])
+                    self.message += " " + json.dumps(self.response_json['data'])
                 except ValueError:
                     pass
+        else:
+            self.message = response.content
 
-        self.status_code = response.status_code
+        self.status_code = response.status
         self.response = response
         self.request = getattr(response, 'request', None)
 
     def __str__(self):
-        if self.request_exception is not None:
-            return 'KucoinException: {}'.format(self.request_exception)
-        else:
-            return 'KucoinException {} -> {}'.format(self.code, self.message)
+        return 'KucoinException {} -> {}'.format(self.code, self.message)
 
 
 class WebSockets:
